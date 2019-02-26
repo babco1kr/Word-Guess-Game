c909e84320f24b27460f3ee1973e952a99b3a0a4
Kyle Babcock <kylerbabcock@gmail.com>
2019-02-25 21:08:16 -0500

    basic and some working java script added, screen posts letter pressed but can't count down attempts remaining

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+var game = ["Holy", "Shadow", "Assassination", "Frost", "Dagger", "Sword", "Shield",
+            "Melee", "Ranged", "Tank", "Healer", "Raid", "Dungeon", "Arena", "Buff",
+            "Debuff", "Discipline", "Destruction", "Fury", "Survival", "Unholy", 
+            "Sylvannas", "Void", "Ashara", "Azeroth"];
+
+// console.log(game.length);
+
+var chosen = Math.floor(Math.random()*25);
+var selection = game[chosen];
+var selectionLength = selection.length;
+var dashes = [selectionLength];
+var attempts = selectionLength +2;
+var letters = selection.split("");
+
+// console.log(selectionLength);
+
+console.log(selection);
+console.log(letters[2]);
+
+
+for (i = 0; i < selectionLength; i++) {
+    document.write("-");
+}
+
+
+
+for (i = attempts; i > 0; i--) {
+document.onkeypress = function(event) {
+    var letterGuessed = String.fromCharCode(event.keyCode);
+    document.getElementById("guess").innerHTML = letterGuessed;
+    document.getElementById("remaining").innerHTML =  i;
+}
+}
