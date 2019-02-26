c92998184129b110429887df05904a83845d0800
Kyle Babcock <kylerbabcock@gmail.com>
2019-02-26 18:32:40 -0500

    Game mostly functioning, need to adjust countdown timer so it doesnt go below 0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
-var game = ["Holy", "Shadow", "Assassination", "Frost", "Dagger", "Sword", "Shield",
-            "Melee", "Ranged", "Tank", "Healer", "Raid", "Dungeon", "Arena", "Buff",
-            "Debuff", "Discipline", "Destruction", "Fury", "Survival", "Unholy", 
-            "Sylvannas", "Void", "Ashara", "Azeroth"];
+var game = ["HOLY", "SHADOW", "ASSASSINATION", "FROST", "DAGGER", "SWORD", "SHIELD",
+            "MELEE", "RANGED", "TANK", "HEALER", "RAID", "DUNGEON", "ARENA", "BUFF",
+            "DEBUFF", "DISCIPLINE", "DESTRUCTION", "FURY", "SURVIVAL", "UNHOLY", 
+            "SYLVANAS", "VOID", "AZSHARA", "AZEROTH"];
 
 // console.log(game.length);
 
@@ -9,33 +9,62 @@ var chosen = Math.floor(Math.random()*25);
 var selection = game[chosen];
 var selectionLength = selection.length;
 var dashes = [];
-var attempts = selectionLength +2;
+var attempts = selectionLength +3;
 var letters = selection.split("");
 var guesses = "";
+var display = "";
 
 // console.log(selectionLength);
 
 console.log(selection);
 console.log(letters[2]);
 
-function showWord(arr){
-for (i = 0; i < arr.length; i++) {
-    dashes [i] = "-";
-    document.write(dashes[i]);
+function start()  {
+    for (i = 0; i < letters.length; i++) {
+        dashes [i] = "_ ";
+        display = display + dashes[i];
+        document.getElementById("game").innerHTML = display;
+        document.getElementById("remaining").innerHTML = attempts;
+    }
+    display = "";
 }
+
+window.onload = function (){
+    start();
 };
 
+function check (item) {
+    for (i = 0; i < letters.length; i++) {
+        if (item == letters[i]) {
+            dashes[i] = item;
+        }
+        display = display + dashes[i];
+        }
+        document.getElementById("game").innerHTML = display;
+        display = "";
+}
+
+function guessMade (x) {
+    guesses = guesses + " " + x;
+    document.getElementById("guess").innerHTML = guesses;
+    
+}
 
 console.log(dashes);
 
-// for (i = attempts; i > 0; i--) {
-//     document.getElementById("remaining").innerHTML =  i;
-// }
 
-showWord (letters);
 
 document.onkeypress = function(event) {
-    var letterGuessed = String.fromCharCode(event.keyCode);
-    guesses = guesses + letterGuessed;
-    document.getElementById("guess").innerHTML = guesses;
+    // defines users key pressed
+    var letterGuessed = event.key;
+    guessMade(letterGuessed);
+    attempts--;
+    // Displays the remaining attempts in the html
+    document.getElementById("remaining").innerHTML = attempts;
+    // For loop that runs the letter through the array of the word looking for matches
+    check(letterGuessed.toUpperCase());
+
+    if (attempts  < 1) {
+        start();
+    }
 };
